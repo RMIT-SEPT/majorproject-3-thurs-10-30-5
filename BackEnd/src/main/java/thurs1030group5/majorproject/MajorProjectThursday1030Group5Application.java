9531f3cda749ab27fca7b9862a2b09d3863873bd
s3541742 <s3541742@student.rmit.edu.au>
2020-08-13 12:34:27 +1000

    Creation of SpringBoot Project

---
+++ BackEnd/src/main/java/thurs1030group5/majorproject/MajorProjectThursday1030Group5Application.java
@@ -0,0 +1,13 @@
+package thurs1030group5.majorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorProjectThursday1030Group5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajorProjectThursday1030Group5Application.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/thurs1030group5/majorproject/MajorProjectThursday1030Group5ApplicationTests.java
@@ -0,0 +1,13 @@
+package thurs1030group5.majorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorProjectThursday1030Group5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
