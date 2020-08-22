e89ddeace285b07c0248b308be1730c081c5977c
s3541742 <s3541742@student.rmit.edu.au>
2020-08-22 15:00:08 +1000

    Changed save method

--- BackEnd/src/main/java/thurs1030group5/majorproject/services/UserService.java
+++ BackEnd/src/main/java/thurs1030group5/majorproject/services/UserService.java
@@ -35,8 +35,9 @@ public class UserService {
     public User saveUser(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setActive(true);
-        Role userRole = roleRepository.findByRole("ADMIN");
-        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+//        FOR WHEN ROLES ARE ADDED
+//        Role userRole = roleRepository.findByRole("ADMIN");
+//        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         return userRepository.save(user);
     }
 
