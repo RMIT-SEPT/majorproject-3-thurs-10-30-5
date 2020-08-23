1fcaebd1818ac9e3e9852a204759b5a56abe9369
s3541742 <s3541742@student.rmit.edu.au>
2020-08-23 21:59:19 +1000

    Moved BCrypt Bean

--- BackEnd/src/main/java/thurs1030group5/majorproject/configuration/SecurityConfiguration.java
+++ BackEnd/src/main/java/thurs1030group5/majorproject/configuration/SecurityConfiguration.java
@@ -1,17 +1,21 @@
 package thurs1030group5.majorproject.configuration;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import thurs1030group5.majorproject.services.UserDataService;
 
 @Configuration
 public class SecurityConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 }
+
+
+
+
