8990655d7f1d375e385292c0539332e906e19dd2
MarcoNadalin <marconadalin00@gmail.com>
2020-08-24 09:19:57 +1000

    Added home component.

--- FrontEnd/app/src/App.js
+++ FrontEnd/app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar.jsx';
+import Home from './components/Home.jsx';
 import Footer from './components/Footer.jsx';
 
 import Image from 'react-bootstrap/Image';
@@ -9,14 +10,8 @@ import Image from 'react-bootstrap/Image';
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      
-      <div class="hero">
-          <img id = 'heroImage' src="https://www.securityroundtable.org/wp-content/uploads/2017/08/AdobeStock_105389989.jpeg"/>     
-          <h2><span>Software Engineering Processes and Tools Team 5</span></h2>
-          <h3><span>Welcome to the basic homepage for the SEPT Team 5.</span></h3>
-      </div>
-
+      <NavBar/>      
+      <Home />
       <Footer/>
     </div>
   );
---
+++ FrontEnd/app/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div class="hero">
+            <img id = 'heroImage' src="https://www.securityroundtable.org/wp-content/uploads/2017/08/AdobeStock_105389989.jpeg"/>     
+            <h2><span>Software Engineering Processes and Tools Team 5</span></h2>
+            <h3><span>Welcome to the basic homepage for the SEPT Team 5.</span></h3>
+        </div>
+    )
+}
