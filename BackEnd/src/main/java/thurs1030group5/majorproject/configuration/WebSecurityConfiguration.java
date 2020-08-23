115c1db07ef174565652a367126fd9abfa2acd25
s3541742 <s3541742@student.rmit.edu.au>
2020-08-23 22:02:22 +1000

    Migrated BCrypt bean

--- BackEnd/src/main/java/thurs1030group5/majorproject/configuration/WebSecurityConfiguration.java
+++ BackEnd/src/main/java/thurs1030group5/majorproject/configuration/WebSecurityConfiguration.java
@@ -1,14 +1,24 @@
 package thurs1030group5.majorproject.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import thurs1030group5.majorproject.services.UserDataService;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+    @Autowired
+    UserDataService userDataService;
 
 //    USED TO CONFIGURE WHICH PAGES CAN BE ACCESSED
     @Override
@@ -18,9 +28,18 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll();
     }
 
-    @Bean()
+
+
+    @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
