3ecb138dd53e1d13b3073d3ee2d6f4fed0f2f66f
s3541742 <s3541742@student.rmit.edu.au>
2020-08-23 22:28:51 +1000

    Added getters and setters

--- BackEnd/src/main/java/thurs1030group5/majorproject/model/Business.java
+++ BackEnd/src/main/java/thurs1030group5/majorproject/model/Business.java
@@ -13,4 +13,27 @@ public class Business {
     private String name;
     private int ownerId;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
 }
