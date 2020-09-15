6497240b1bee1a9e65500885421017a080caf21c
s3541742 <s3541742@student.rmit.edu.au>
2020-09-15 23:30:51 +1000

    Added crossOrigin

--- BackEnd/src/main/java/thurs1030group5/majorproject/web/BusinessController.java
+++ BackEnd/src/main/java/thurs1030group5/majorproject/web/BusinessController.java
@@ -2,6 +2,7 @@ package thurs1030group5.majorproject.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import thurs1030group5.majorproject.model.Business;
 import thurs1030group5.majorproject.services.BusinessService;
 
 import java.util.List;
-
+@CrossOrigin("*")
 @RestController
 public class BusinessController {
 
