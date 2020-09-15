75812564887722c739ff0ea5f35e2f938f22067e
s3541742 <s3541742@student.rmit.edu.au>
2020-09-15 21:18:30 +1000

    Added initial UserService Unit Tests

---
+++ BackEnd/src/test/java/thurs1030group5/majorproject/services/UserServiceTest.java
@@ -0,0 +1,50 @@
+package thurs1030group5.majorproject.services;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import thurs1030group5.majorproject.DTO.UserDTO;
+import thurs1030group5.majorproject.model.User;
+import thurs1030group5.majorproject.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        User testUser = new User();
+        testUser.setUsername("UserTest");
+        UserDTO testUserDTO = new UserDTO(testUser);
+        Mockito.when(userRepository.findByUsername(testUserDTO.getUsername())).thenReturn(testUser);
+        Mockito.when(userRepository.findByUsername("invalidUsername")).thenReturn(null);
+    }
+    @Test
+    public void whenValidUsername_thenUserShouldBeFound() {
+        String username = "UserTest";
+        UserDetails foundUser = userService.loadUserByUsername(username);
+        assertEquals(username, foundUser.getUsername());
+    }
+
+    @Test
+    public void whenInvalidUsername_thenUsernameNotFoundExceptionThrown() {
+        String username = "invalidUsername";
+        assertThrows(UsernameNotFoundException.class, () -> userService.loadUserByUsername(username));
+
+    }
+
+}
