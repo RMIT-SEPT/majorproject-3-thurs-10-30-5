5788cb3a3db8e3e9e122debf3f47560b037ec41f
s3779722 <s3779722@student.rmit.edu.au>
2020-09-15 16:24:04 +1000

    Added use and to elements back into card button.

--- FrontEnd/app/src/components/CardComponent/CardComponent.jsx
+++ FrontEnd/app/src/components/CardComponent/CardComponent.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-
 import { Box, Card, Button, Image } from 'bumbag';
 
 function CardComponent({ name, button, description, buttonlink }) {
@@ -11,7 +10,7 @@ function CardComponent({ name, button, description, buttonlink }) {
       height="320px"
       title={name}
       footer={
-        <Button width="150px" palette="primary" textAlign="center">
+        <Button width="150px" palette="primary" textAlign="center" use={RouterLink} to="/">
           {button}
         </Button>
       }
@@ -20,7 +19,7 @@ function CardComponent({ name, button, description, buttonlink }) {
       <Image
         maxWidth="180px"
         src="https://cdn3.wpbeginner.com/wp-content/uploads/2020/03/ultimate-small-business-resource-coronavirus.png"
-      />
+      /> 
       <Box>{description}</Box>
     </Card>
   );
--- FrontEnd/app/src/components/CardComponent/__test__/Card.test.js
+++ FrontEnd/app/src/components/CardComponent/__test__/Card.test.js
@@ -4,23 +4,22 @@ import CardComponent from '../CardComponent.jsx';
 import "@testing-library/jest-dom/dist/extend-expect";
 import "@testing-library/jest-dom/dist/to-have-text-content";
 import { StaticRouter } from 'react-router-dom';
+import { render, cleanup } from '../../../Utils/TestUtils.jsx';
 
-import { render, cleanup } from "@testing-library/react";
-
+//RAYMOND'S TESTS
+//-----------------------------------------------------------------------------
 //cleanup after each test
 afterEach(cleanup);
 
 //test if the website renders without crashing the website
-it("Render without crashing the website", ()=>{
-    const div = document.createElement("div");
-    ReactDOM.render(<StaticRouter location="/">
-                        <CardComponent  name="Test" button="Test" description="Test"></CardComponent>
-                    </StaticRouter>, div);
-})
+test('Render without crashing the website', () => {
+    expect(() => render(<StaticRouter><CardComponent  name="Test" button="Test" description="Test"></CardComponent></StaticRouter>)).not.toThrowError();
+});
 
 //test if the props are rendered correctly
 it("Renders props correctly", ()=>{
-    const { getByTestId } = render(<CardComponent  name="Test1" button="Test1" description="Test1"></CardComponent>)
+    const { getByTestId } = render(<StaticRouter><CardComponent  name="Test1" button="Test1" description="Test1"></CardComponent></StaticRouter>)
     expect(getByTestId("card-test")).toHaveTextContent("Test1");
-})
+});
+//-----------------------------------------------------------------------------
 
