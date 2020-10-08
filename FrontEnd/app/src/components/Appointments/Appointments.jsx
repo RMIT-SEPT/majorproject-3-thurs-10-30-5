a37be648bc707807df5fb64c816a0aa460a5d139
s3779722 <s3779722@student.rmit.edu.au>
2020-10-08 12:38:59 +1100

    Added cards to Appointments.jsx

--- FrontEnd/app/src/components/Appointments/Appointments.jsx
+++ FrontEnd/app/src/components/Appointments/Appointments.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PageContent, Card, Paragraph } from 'bumbag';
+import { PageContent, Card, Heading, Paragraph } from 'bumbag';
 import useUser from '../../hooks/useUser';
 import axios from 'axios';
 import * as Loads from 'react-loads';
@@ -30,12 +30,32 @@ export default function Appointments() {
     let bookings = bookingsRecord.response || [];
     
     return (
-        <PageContent>
+        <PageContent paddingBottom="350px" paddingRight="30px" paddingLeft="30px" height="100%">
+        <Heading fontWeight="bold" paddingBottom="20px">Your Appointments</Heading>
         {bookings.map(booking => (
-            <Paragraph>
-                Id: {booking.id}, 
-                Booking with {booking.worker.firstName + " " + booking.worker.lastName} on {booking.appointment.appointmentTime}
-            </Paragraph>
+            <Card marginBottom = "15px">
+                <Card.Header >
+                    <span>
+                        <Heading use="h4" color="steelblue">
+                            Booking with {booking.worker.firstName + " " + booking.worker.lastName}
+                        </Heading>
+                    </span> 
+                    <span>
+                        <Paragraph fontStyle="italic">
+                            {booking.appointment.appointmentTime}
+                        </Paragraph>
+                    </span>
+                </Card.Header>
+
+                <Card.Content>
+                    <Heading use="h6" color="darkgray">
+                        Details:
+                    </Heading>
+                    <Paragraph width="52%">
+                         {booking.appointment.description}
+                    </Paragraph>
+                </Card.Content>
+            </Card>               
           ))}
         </PageContent>
     )
