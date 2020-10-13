d616e14d7d51b2be8d78e53bb3abd2be878d7460
MarcoNadalin <marconadalin00@gmail.com>
2020-10-14 08:43:09 +1100

    Improved signup page to now provide the information required from the backend, as it has recently been updated. Also improved the services page to look more polished. The service page also redirects the user to their appointments page once they have correctly made a booking.

--- FrontEnd/app/src/components/Services.jsx
+++ FrontEnd/app/src/components/Services.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
-import { Box, Button, Heading, PageContent, Card, FieldStack, SelectMenuField, Alert, Label } from 'bumbag';
+import { useHistory } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Heading,
+  PageContent,
+  Card,
+  FieldStack,
+  SelectMenuField,
+  Alert,
+  Label,
+  InputField,
+  Paragraph
+} from 'bumbag';
+
 import * as Loads from 'react-loads';
 import useUser from '../hooks/useUser';
 import axios from 'axios';
@@ -15,6 +29,8 @@ export default function Services() {
   const [dateValue, setDateValue] = React.useState();
   //Initializing custom react hook useUser
   const { user } = useUser();
+
+  const history = useHistory();
 
   //////////////////////////////////////////////////////////////////
   //=========================SERVICES=============================//
@@ -131,9 +147,13 @@ export default function Services() {
     };
     console.log('booking: ', data);
     //Make the axios POST request, using the data object created earlier, and the headers.
-    const response = await axios.post('http://localhost:8080/api/booking/create', JSON.stringify(data), options);
+    const response = await axios
+      .post('http://localhost:8080/api/booking/create', JSON.stringify(data), options)
+      .then(res => {
+        history.push('/appointments');
+      })
+      .catch(err => {});
 
-    console.log(response);
     return response;
   };
 
@@ -170,8 +190,8 @@ export default function Services() {
         <Heading textAlign="center"> Services </Heading>
         <SelectMenuField
           hasSearch
-          width="80vh"
-          label="Select a Service"
+          width="100%"
+          label="Step 1: Select a Service"
           onChange={setServiceValue}
           options={serviceList}
           placeholder="Select a Service"
@@ -193,36 +213,41 @@ export default function Services() {
                   </Heading>
                   <SelectMenuField
                     width="100%"
-                    label="Select your preferred worker"
+                    label="Step 2: Select your preferred worker"
                     onChange={setWorkerValue}
                     options={workers}
                     placeholder="Select your desired worker..."
                     value={workerValue}
-                    paddingBottom="20px"
+                    paddingBottom="50px"
                   />
-                  <Label>Select your appointment date</Label>
-                  <Box width="100%">
-                    <input
-                      type="date"
-                      id="start"
-                      name="appointment-date"
-                      value={dateValue}
-                      min="2020-10-10"
-                      max="2030-1-1"
-                      onChange={e => setDateValue(e.target.value)}
-                      disabled={workerValue ? false : true}
-                    />
-                  </Box>
-
+                  <InputField
+                    label="Step 3: Select your appointment date"
+                    type="date"
+                    id="start"
+                    name="appointment-date"
+                    value={dateValue}
+                    min="2020-10-10"
+                    max="2030-1-1"
+                    onChange={e => setDateValue(e.target.value)}
+                    disabled={workerValue ? false : true}
+                    paddingBottom="50px"
+                  />
                   <SelectMenuField
                     width="100%"
-                    paddingBottom="10px"
-                    label="Available Times"
+                    label="Step 4: Select your preferred time that the worker is available"
                     onChange={setValue}
                     options={availability}
                     placeholder="Select a day to book your appointment..."
                     value={value}
+                    disabled={dateValue && workerValue ? false : true}
+                    paddingBottom="100px"
                   />
+                  {serviceValue.label && dateValue && value && (
+                    <Paragraph paddingBottom="50px">
+                      You are about to make a booking with {serviceValue.label} on {dateValue} at {value.value}. Is this
+                      information correct? If so, please press the Book Now button below.
+                    </Paragraph>
+                  )}
                   <Button palette="primary" width="100%" margin="auto" onClick={createBooking}>
                     Book Now!
                   </Button>
